3532f12a668bd186d2350afd7264f16923a5ecfb
Anders Christiansen Sørby <anders@sorby.xyz>
2021-06-29 15:27:18 +0200

    Add flake and nix CI

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Introit the standard lib of Yatima";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.yatima.url = "github:yatima-inc/yatima/acs/opts-to-cli";
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, utils, yatima }: 
+    utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      yatima-bin = yatima.apps.${system}.yatima.program;
+      name = "introit";
+      src = ./.;
+      yatima-check = file: pkgs.runCommand "yatima-check" {
+        inherit system src;
+        buildInputs = [ yatima ];
+      } ''
+        ${yatima-bin} --no-file-store --root ${src} check ${file} > $out
+      '';
+
+    in
+    {
+      checks.${name} = yatima-check "introit.ya";
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          yatima
+        ];
+      };
+
+    });
+}
